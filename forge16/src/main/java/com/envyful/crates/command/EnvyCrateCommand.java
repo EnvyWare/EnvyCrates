28fe9e5f903b816cfaff71ca75c59b1bd749ea7f
Daniel Norris <dnorris443@gmail.com>
2022-10-11 02:57:08 +0100

    feat(forge16): add root crates command

---
+++ forge16/src/main/java/com/envyful/crates/command/EnvyCrateCommand.java
@@ -0,0 +1,28 @@
+package com.envyful.crates.command;
+
+import com.envyful.api.command.annotate.Command;
+import com.envyful.api.command.annotate.Permissible;
+import com.envyful.api.command.annotate.executor.CommandProcessor;
+import com.envyful.api.command.annotate.executor.Sender;
+import com.envyful.api.forge.chat.UtilChatColour;
+import com.envyful.crates.EnvyCrates;
+import net.minecraft.command.ICommandSource;
+import net.minecraft.util.Util;
+
+@Command(
+        value = "envycrates",
+        description = "Root crates command",
+        aliases = {
+                "crates"
+        }
+)
+@Permissible("com.envyful.crates.command")
+public class EnvyCrateCommand {
+
+    @CommandProcessor
+    public void onCommand(@Sender ICommandSource sender, String[] args) {
+        for (String s : EnvyCrates.getInstance().getLocale().getRootCommand()) {
+            sender.sendMessage(UtilChatColour.colour(s), Util.NIL_UUID);
+        }
+    }
+}
