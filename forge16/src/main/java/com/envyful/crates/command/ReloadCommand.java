5e08ca50ef726451e206530d5b1422cd95232470
Daniel Norris <dnorris443@gmail.com>
2022-11-04 08:12:19 +0000

    fix(command): add feedback to reload command

--- forge16/src/main/java/com/envyful/crates/command/ReloadCommand.java
+++ forge16/src/main/java/com/envyful/crates/command/ReloadCommand.java
@@ -5,9 +5,11 @@ import com.envyful.api.command.annotate.Command;
 import com.envyful.api.command.annotate.Permissible;
 import com.envyful.api.command.annotate.executor.CommandProcessor;
 import com.envyful.api.command.annotate.executor.Sender;
+import com.envyful.api.forge.chat.UtilChatColour;
 import com.envyful.crates.EnvyCrates;
 import com.envyful.crates.type.crate.CrateTypeFactory;
 import net.minecraft.command.ICommandSource;
+import net.minecraft.util.Util;
 
 @Command(
         value = "reload",
@@ -24,5 +26,6 @@ public class ReloadCommand {
     public void onCommand(@Sender ICommandSource sender, String[] args) {
         EnvyCrates.getInstance().reloadConfig();
         CrateTypeFactory.reload();
+        sender.sendMessage(UtilChatColour.colour("&e&l(!) &eReloaded configs & crates."), Util.NIL_UUID);
     }
 }
